fd6084d07223b8b85659d727b49924b4257aaba7
Javier <javimacias@gmail.com>
2024-10-30 18:13:05 +0100

    mejoras en pantalla inicio

--- src/components/PantallaInicio.jsx
+++ src/components/PantallaInicio.jsx
@@ -1,19 +1,26 @@
-// src/components/PantallaInicio.jsx
 import React, { useState } from "react";
 import "./PantallaInicio.css";
 
 const PantallaInicio = ({ onIniciarJuego }) => {
-  const [numJugadores, setNumJugadores] = useState(0);
+  const [numJugadores, setNumJugadores] = useState("");
+  const [error, setError] = useState("");
 
   const manejarCambioJugadores = (e) => {
-    setNumJugadores(parseInt(e.target.value) || 0);
+    const value = parseInt(e.target.value) || "";
+    setNumJugadores(value);
+
+    if (value < 2 || value > 4) {
+      setError("Por favor, selecciona entre 2 y 4 jugadores.");
+    } else {
+      setError("");
+    }
   };
 
   const iniciarJuego = () => {
     if (numJugadores >= 2 && numJugadores <= 4) {
       onIniciarJuego(numJugadores); // Llama a la función de inicio de juego con el número de jugadores seleccionado
     } else {
-      alert("Por favor, selecciona entre 2 y 4 jugadores.");
+      setError("Por favor, selecciona entre 2 y 4 jugadores.");
     }
   };
 
@@ -26,10 +33,12 @@ const PantallaInicio = ({ onIniciarJuego }) => {
           type="number"
           min="2"
           max="4"
+          placeholder="Número de jugadores"
           value={numJugadores}
           onChange={manejarCambioJugadores}
         />
         <button onClick={iniciarJuego}>Iniciar Juego</button>
+        {error && <p className="error">{error}</p>}
       </div>
     </div>
   );
