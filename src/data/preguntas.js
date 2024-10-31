de8d0ca4bb37554cf3ccfad664c38eb4d4f85414
Javier <javimacias@gmail.com>
2024-10-31 13:40:17 +0100

    nuevas preguntas

--- src/data/preguntas.js
+++ src/data/preguntas.js
@@ -1,7 +1,9 @@
 // src/data/preguntas.js
 
 const preguntas = [
+    
     { pregunta: "¿En qué año se lanzó la película E.T.: '1983', '1979', '1980' o '1982'?", respuesta: "1982", imagen: null, music: null },
+    { pregunta: "¿Para que pelicula compuso Queen esta BSO? " , respuesta: "Los inmortales", imagen: null, music: "/music/inmortales.mp3" },
     { pregunta: "¿Qué episodio de Star Wars se estrenó en España en 1980: Episodio IV, Episodio V o Episodio VI?", respuesta: "Episodio V", imagen: "/images/EpisodioV.jpg", music: null },
     { pregunta: "¿En qué año se celebró Live Aid: 1983, 1985, 1987 o 1989?", respuesta: "1985", imagen: null, music: null },
     { pregunta: "¿Qué grupo musical tocaba esta canción?", respuesta: "Dire Straits", imagen: null, music: "/music/DireStraits.mp3" },
@@ -12,9 +14,17 @@ const preguntas = [
     { pregunta: "¿Que cantante se encumbró en 1984 cantando como una virgen?", respuesta: "Madonna", imagen: null, music: null },
     { pregunta: "¿Que presidente americano recibio un disparo en 1981: Bush, Carter, Reagan, Clinton", respuesta: "Reagan", imagen: null, music: null },
     { pregunta: "¿Como se llama el grupo que canta esto y comenzó sus pasos en 1980?", respuesta: "los secretos", imagen: null, music: "/music/secetos.mp3" },
-    { pregunta: "¿Cual era la moneda de Rumanía en los años 80: tolar, franco, lira o leu ", respuesta: "tolar", imagen: null, music: null },
-    { pregunta: "¿Quien interpretó 'Juegos de Guerra': 'M.Broderick', 'R.Dowmey Jr', 'W.Wills' o 'J.Dante'", respuesta: "M.Broderick", imagen: null, music: "/images/juegosguerra" },
+    { pregunta: "¿Cual era la moneda de Eslovenia en los años 80: tolar, franco, lira o leu ", respuesta: "tolar", imagen: null, music: null },
+    { pregunta: "¿Quien interpretó 'Juegos de Guerra': 'M.Broderick', 'R.Dowmey Jr', 'W.Wills' o 'J.Dante'", respuesta: "M.Broderick", imagen: "/images/juegosguerra.jpg", music: null },
     { pregunta: "¿Como se llamaba el perro de Indiana Jones: Bob, Indiana, Rex, orejitas", respuesta: "indiana", imagen: null, music: null },
+    { pregunta: "¿Como se llamaba el saltamontes de 'la abeja maya': 'clip', 'flas', 'patas', 'flip'", respuesta: "flip", imagen: null, music: null },
+    { pregunta: "¿Quien ha compuesto esta banda sonora: 'J. Williams', 'D. Elfman', 'J. Barry', 'H.Marcini'" , respuesta: "J. Williams", imagen: null, music: "/music/superman.mp3" },
+    { pregunta: "¿Que BSO no es de J. Williams: 'Superman', 'Star Wars', 'Tiburon', 'indiana Jones', 'Parque Jurasico', 'Memorias de Africa'", respuesta: "memorias de africa", imagen: null, music: null },
+    { pregunta: "¿Como se llamaba este personaje': 'flap', 'willy', 'patas', 'wally', 'lucas'", respuesta: "willy", imagen: "/images/willy.jpg", music: null },
+    { pregunta: "¿Que coche fue el más vendido en españa en 1982: 'renault 5', 'ford fiesta', 'seat 124', 'citroen BX'", respuesta: "renault 5", imagen: null, music: null },
+    { pregunta: "¿Que jugador sumó más goles en 'la liga' durante toda la decada de los 80: 'Quini', 'Fernando Gomez Colomer', 'Hugo Sanchez', 'Maradona' ", respuesta: "Fernando Gomez Colomer", imagen: null, music: null },
+    { pregunta: "¿Que equipo gano más titulos de Liga en españa en los 80?: Real Madrid, Barcelona, Athletic club, Real Sociedad", respuesta: "Real madrid", imagen: null, music: null },
+    { pregunta: "¿Como se llamaba este personaje amiga de Naranjito: 'Naranjita', 'Amapola', 'Clementina' o 'Margarita'", respuesta: "Clementina", imagen: "/images/clementina.jpg", music: null },
 
 
 
