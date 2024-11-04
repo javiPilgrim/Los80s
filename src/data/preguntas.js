160dec0a42f2bdd21b3ab70ca6b61acb30792200
Javier <javimacias@gmail.com>
2024-11-04 20:05:39 +0100

    preguntas

--- src/data/preguntas.js
+++ src/data/preguntas.js
@@ -3,6 +3,7 @@
 const preguntas = [
     
     { pregunta: "¿En qué año se lanzó la película E.T.?", opciones: ["1983", "1979", "1980", "1982"], respuesta: "1982", imagen: null, music: null},
+    { pregunta: "¿Como sigue la cancion?", opciones: ["Que termine un momento glorioso", "Que termine un momento precioso", "Que termine un momento horroroso", "Que termine un momento grandioso"] , respuesta: "Que termine un momento precioso", imagen: null, music: "/music/heroes.mp3" },
     { pregunta: "¿Como se llamaba esta serie de dibujos animados?", opciones: ["Ulises 21", "Ulises y la Odisea", "Odisea 21", "Ulises 31"] , respuesta: "Ulises 31", imagen: null, music: "/music/ulises.mp3" },
     { pregunta: "¿Como se llamaba la protagonista de 'Top Gun'?", opciones: ["kelly", "Ada", "Wendy", "Charlie"], respuesta: "Charlie", imagen: null, music: null},
     { pregunta: "¿Que país logró su independencia en 1980?", opciones: ["Zimbabue", "Laos", "Burkina Faso", "Zambia"], respuesta: "Zimbabue", imagen: null, music: null},
@@ -28,15 +29,19 @@ const preguntas = [
     { pregunta: "¿Como se llamaba el saltamontes de 'la abeja maya'?", opciones: ["clip", "flas", "patas", "flip"], respuesta: "flip", imagen: null, music: null },
     { pregunta: "¿Quien ha compuesto esta banda sonora?", opciones: ["J. Williams", "D. Elfman", "J. Barry", "H.Marcini"] , respuesta: "J. Williams", imagen: null, music: "/music/superman.mp3" },
     { pregunta: "¿Que BSO no es de J. Williams?", opciones: ["Superman", "Parque Jurásico", "Indiana Jones", "Memorias de Africa"], respuesta: "Memorias de Africa", imagen: null, music: null },
-    { pregunta: "¿En qué año se lanzó la canción *Thriller* de Michael Jackson??", opciones: ["1980", "1982", "1984", "1985"], respuesta: "Indiana", imagen: null, music: null },
+    { pregunta: "¿En qué año se lanzó la canción *Thriller* de Michael Jackson??", opciones: ["1980", "1982", "1984", "1985"], respuesta: "1982", imagen: null, music: null },
     { pregunta: "¿Como se llamaba este personaje'?", opciones: ["flap", "willy", "wally", "lucas"],  respuesta: "willy", imagen: "/images/willy.jpg", music: null },
     { pregunta: "¿En qué año se emitió por primera vez *Los Caballeros del Zodiaco*?", opciones: ["1983", "1986", "1988", "1982"], respuesta: "1986", imagen: null, music: null},
-    { pregunta: "¿Que serie sustituyo a Mazinger Z?", opciones: ["Sandokan", "Salvados por la campana", "El Show de los teleñecos", "OrzoWei"], respuesta: "Orzowei", imagen: null, music: null},
+    { pregunta: "¿Que serie sustituyo a Mazinger Z?", opciones: ["Sandokan", "Salvados por la campana", "El Show de los teleñecos", "Orzowei"], respuesta: "Orzowei", imagen: null, music: null},
     { pregunta: "¿Que coche fue el más vendido en españa en 1982?", opciones: ["Renault 5", "Ford Fiesta", "Seat 124", "Citroen BX"], respuesta: "Renault 5", imagen: null, music: null },
     { pregunta: "¿Que jugador sumó más goles en 'la liga' durante toda la decada de los 80?", opciones: ["Quini", "Fernando Gomez Colomer", "Hugo Sanchez", "Maradona"] , respuesta: "Fernando Gomez Colomer", imagen: null, music: null },
     { pregunta: "¿Con que nombre se dio a conocer el viscoso fantasma de 'Cazafantasmas?", opciones: ["Slimer", "Clingy", "Chubby", "Bussi"], respuesta: "Slimer", imagen: null, music: null},
     { pregunta: "¿Que equipo gano más titulos de Liga en españa en los 80?", opciones: ["Real Madrid", "Athletic club", "Real Sociedad", "Barcelona"], respuesta: "Real Madrid" , imagen: null, music: null },
-
+    { pregunta: "¿Como se llama el robot protagonista de 'Cortocircuito'?", opciones: ["Johnny 3", "Harry 4", "Johnny 5", "Harry 6"], respuesta: "Johnny 5", imagen: null, music: null },
+    { pregunta: "Qué famoso videojuego presentaba a un héroe que debía rescatar a una princesa de un dragón", opciones: ["Monkey Island", "Sim City", "Super Mario Bros", "Prince of Persia"], respuesta: "Super Mario Bros", imagen: null, music: null },
+    { pregunta: "¿Que actor llevó al cine al popular heroe de comics 'He-man'?", opciones: ["Jean-Claude Van Damme", "Dolph Lundgren", "Sivester Stallone", "Arnold Schwarzenegger"], respuesta: "Dolph Lundgren", imagen: "/images/heman.jpg", music: null },
+    { pregunta: "¿Que videojuego tiene esta BSO?", opciones: ["Super Mario Bros", "Donkey Kong", "Tetris", "Space Invader"], respuesta: "Tetris", imagen: null, music: "/music/tetris.mp3" },
+    { pregunta: "¿Cuál es el nombre del famoso lugar donde se celebró el festival de música de Woodstock en 1969, que tuvo un renacimiento en los años 80?", opciones: ["Rosstrick", "St. Louise", "Springs", "Bethel"], respuesta: "Bethel", imagen: null, music: null },
 
 
 
