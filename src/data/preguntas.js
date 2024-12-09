a80cbc9004af263ee12d7bc464284991934d4e34
javimacias@gmail.com <javimacias@gmail.com>
2024-12-09 10:55:27 +0100

    preguntas

--- src/data/preguntas.js
+++ src/data/preguntas.js
@@ -446,6 +446,12 @@ const preguntas = [
  { pregunta: "¿Cual fue el primer largometraje en incluir el primer personaje fotorealista generado enteramente por ordenador?", opciones: ["Abiss", "Tron", "El secreto de la pirámide", "Willow"], respuesta: "El secreto de la pirámide", music: null, imagen: null },
  { pregunta: "¿Qué película inmortalizo la canción 'Heart and Soul' de Floyd Cramer", opciones: ["Willow", "Flashdance", "Cortocircuito", "Big"], respuesta: "Big", music: "/music/big.mp3", imagen: null },
  { pregunta: "¿Qué famoso locutor de radio era conocido por frases como 'Hola, hola, hola..', 'Tu y yo lo sabíamos..', '3, 2 o 1', 'Besitos para ellas, abrazos para ellos?", opciones: ["Joaquín Luqui", "Miguel Abellán", "Fernandisco", "El pulpo"], respuesta: "Joaquín Luqui", music: null, imagen: null },
+ { pregunta: "¿Cual de estos cometas nos visitó en los 80'?", opciones: ["Hale-Bopp", "Halley", "Shoemaker-Levy 9", "Atlas"], respuesta: "Halley", music: null, imagen: null },
+ { pregunta: "¿Cual de estos personajes NO fue colaborador del programa 'Protagonistas' de Luis del Olmo?", opciones: ["Antonio Mingote", "Cumy Chumez", "Miguel Gila", "Luis Sánchez Polack"], respuesta: "Miguel Gila", music: null, imagen: "/images/luisdelolmo.jpeg" },
+ { pregunta: "En 1981 hizo su primer vuelo el primer transbordador que podía ser lanzado al espacio más de una vez ¿Cual era su nombre?", opciones: ["Columbia", "Challenguer", "Atlantis", "Discovery"], respuesta: "Columbia", music: null, imagen: null },
+ { pregunta: "En 1982 se produce un hito en la medicina moderna en la persona del doctor Barney Clarck. ¿De que hecho hablamos?", opciones: ["primer trasplante de cornea", "primer trasplante pancreático", "primer trasplante de celulas de medula osea", "primer trasplante de corazón artificial"], respuesta: "primer trasplante de corazón artificial", music: null, imagen: null },
+ { pregunta: "¿Que figura artística murió el 23 de enero de 1989?", opciones: ["Salvador Dalí", "Pablo Picasso", "Joan Miró", "Ignacio Zuloaga"], respuesta: "Salvador Dalí", music: null, imagen: "/images/dali.jpg" },
+ { pregunta: "¿Que figura artística murió el 23 de enero de 1983?", opciones: ["Salvador Dalí", "Pablo Picasso", "Joan Miró", "Ignacio Zuloaga"], respuesta: "Joan Miró", music: null, imagen: "/images/miro.jpg" },
 
 
   ];
